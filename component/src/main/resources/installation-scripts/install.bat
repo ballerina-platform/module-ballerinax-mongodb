4d889dc9ff0503a2044b7f4adc08e74b8c432235
Manuri <amaya.uom@gmail.com>
2018-06-27 23:04:37 +0530

    Improve install.bat

--- component/src/main/resources/installation-scripts/install.bat
+++ component/src/main/resources/installation-scripts/install.bat
@@ -20,7 +20,7 @@ IF EXIST "%ballerina_lib_location%\wso2-%package_name%-package-%version%.jar" (
 
 XCOPY ".\dependencies\wso2-%package_name%-package-%version%.jar" "%ballerina_lib_location%" /y
 
-IF %ERRORLEVEL% GTR 1 (
+IF %ERRORLEVEL% GTR 0 (
     ECHO An error occurred while copying .\dependencies\wso2-%package_name%-package-%version%.jar to %ballerina_lib_location%
 	ECHO Installation unsuccessful.
 	GOTO :FAILED
@@ -29,17 +29,31 @@ IF %ERRORLEVEL% GTR 1 (
 rem Create directory hierarchy for the balo if it doesn't already exist
 IF NOT EXIST "%ballerina_balo_location%\wso2\%package_name%\0.0.0" (
     MKDIR "%ballerina_balo_location%\wso2\%package_name%\0.0.0"
+
+	IF ERRORLEVEL 1 (
+		ECHO An error occurred while copying .\balo\wso2\%package_name%\0.0.0\%package_name%.zip to %ballerina_balo_location%\wso2\%package_name%\0.0.0
+		ECHO Installation unsuccessful. Reverting changes.
+		IF EXIST "temp\wso2-%package_name%-package-%version%.jar" (
+			ECHO Copying backed-up wso2-%package_name%-package-%version%.jar to %ballerina_lib_location%
+			XCOPY "temp\wso2-%package_name%-package-%version%.jar" "%ballerina_lib_location%" /y
+		) ELSE (
+			DEL "%ballerina_lib_location%\wso2-%package_name%-package-%version%.jar"
+		)
+		GOTO :FAILED
+    )
 )
 
 rem Copy balo
 XCOPY ".\balo\wso2\%package_name%\0.0.0\%package_name%.zip" "%ballerina_balo_location%\wso2\%package_name%\0.0.0" /e /y
 
-IF %ERRORLEVEL% GTR 1 (
+IF %ERRORLEVEL% GTR 0 (
     ECHO An error occurred while copying .\balo\wso2\%package_name%\0.0.0\%package_name%.zip to %ballerina_balo_location%\wso2\%package_name%\0.0.0
     ECHO Installation unsuccessful. Reverting changes.
     IF EXIST "temp\wso2-%package_name%-package-%version%.jar" (
 	    ECHO Copying backed-up wso2-%package_name%-package-%version%.jar to %ballerina_lib_location%
 	    XCOPY "temp\wso2-%package_name%-package-%version%.jar" "%ballerina_lib_location%" /y
+	) ELSE (
+	    DEL "%ballerina_lib_location%\wso2-%package_name%-package-%version%.jar"
 	)
 	GOTO :FAILED
 )
@@ -54,7 +68,7 @@ ECHO 1. dependencies\wso2-%package_name%-package-%version%.jar to %ballerina_lib
 ECHO 2. balo\wso2\%package_name%\0.0.0\%package_name%.zip to %ballerina_balo_location%\wso2\%package_name%\0.0.0
 
 :END
-IF EXIST ./temp (
+IF EXIST .\temp (
 RD temp /s /q
 )
 ENDLOCAL
