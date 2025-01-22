c5b8624a825edd71e6ccc66db04ba46499d27e2f
Ravin Perera <62183347+ravinperera00@users.noreply.github.com>
2025-01-22 16:55:55 +0530

    Migrate to Java 21 (#258)
    * Migrate to Java 21
    
    * Update build.gradle and graalvm workflow

--- native/src/main/java/io/ballerina/lib/mongodb/IteratorUtils.java
+++ native/src/main/java/io/ballerina/lib/mongodb/IteratorUtils.java
@@ -19,10 +19,10 @@
 package io.ballerina.lib.mongodb;
 
 import com.mongodb.client.MongoCursor;
-import io.ballerina.runtime.api.PredefinedTypes;
 import io.ballerina.runtime.api.creators.ErrorCreator;
 import io.ballerina.runtime.api.creators.TypeCreator;
 import io.ballerina.runtime.api.creators.ValueCreator;
+import io.ballerina.runtime.api.types.PredefinedTypes;
 import io.ballerina.runtime.api.types.Type;
 import io.ballerina.runtime.api.types.UnionType;
 import io.ballerina.runtime.api.values.BError;
